0004555ae92ad7a6a403d7391c0bde1ca0073c0d
Jesus Francisco CAstrejón Canales <franck_768@hotmail.com>
2025-05-09 12:32:44 -0600

    remove import not used

--- app/(app)/horarios/create/page.tsx
+++ app/(app)/horarios/create/page.tsx
@@ -2,13 +2,6 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { useState } from "react";
