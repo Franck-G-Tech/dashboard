e555f37b656742b9b2b5e12c4e9ed09c85d55c68
Jesus Francisco CAstrejón Canales <franck_768@hotmail.com>
2025-05-28 15:33:07 -0600

    test api directa

--- convex/users.ts
+++ convex/users.ts
@@ -11,8 +11,8 @@ import { Resend } from "resend";
 // 1. Inicialización de Clientes Externos (Clerk)
 // ====================================================================
 const clerk = createClerkClient({ secretKey: process.env.CLERK_SECRET_KEY });
-const resend = new Resend(process.env.RESEND_API_KEY);
-//const resend = new Resend("re_74k5dysT_EDi3zUNajjFcy3LRHfnw45nj");
+//const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_74k5dysT_EDi3zUNajjFcy3LRHfnw45nj");
 const emailResend = "School-App <noreply@franck.korian-labs.net>";
 
 // ====================================================================
